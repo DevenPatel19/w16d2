64d31d2e868bf7b705d5b6821d2937b0a2328807
Deven Patel <deven.h.j.patel+softwaredev@gmail.com>
2023-10-17 12:20:25 -0400

    corrected

--- SLLqueue.js
+++ SLLqueue.js
@@ -1,61 +1,76 @@
-class Node {
-    constructor(data) {
-        this.data = data;
-        this.next = null;
+class Node{
+    constructor(data){
+      this.data = data;
+      this.next = null;
     }
-}
-
-/**
- * Class to represent a queue using a SLL to store the queued items.
- * Follows a FIFO (First In First Out) order where new items are added to the
- * back and items are removed from the front.
- */
-class Queue {
-    constructor() {
-        this.front = null;
-        this.rear = null;
+  }
+  
+  
+  
+  
+  /**
+   * Class to represent a queue using a SLL to store the queued items.
+   * Follows a FIFO (First In First Out) order where new items are added to the
+   * back and items are removed from the front.
+   */
+  class Queue{
+    constructor(){
+      this.front = null;
+      this.rear = null;
+      this.size = 0;
     }
-
-    /**
+  
+      /**
      * Returns whether or not this queue is empty.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {boolean}
      */
-    isEmpty() {
-        return this.size === null;
+    isEmpty(){
+      return this.front == null;
     }
-
-    /**
+  
+      /**
      * Retrieves the data of the first item without removing it.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @returns {any} The first item or undefined if empty.
      */
-    getFront() {
-        return this.front ? this.front.data : undefined;
+    getFront(){
+      //if check if empty return undefined if not return front.data
+      if(this.isEmpty()){
+        return undefined;
+      }
+      return this.front.data
     }
-
-    /**
+  
+      /**
      * Adds a new item with the data to the back of this queue.
      * - Time: O(1) constant.
      * - Space: O(1) constant.
      * @param {any} item The new item to add to the back.
      * @returns {number} The new size of this queue.
      */
-    enqueue(data) {
-        const newNode = new Node(data);
-        if (this.isEmpty()) {
-            this.front = newNode;
-            this.rear = newNode;
-        } else {
-            this.rear.next = newNode;
-            this.rear = newNode;
-        }
-        this.size++;
-        return this.size;
+    enqueue(data){
+      // instantiate new node
+      const newNode = new Node(data);
+      // check is queue is empty
+      if(this.isEmpty()){
+        //if empty set newNode as the whole line
+        this.front = newNode;
+        this.rear = newNode;
+      } else {
+        //place the newNode as the item after the rear in queue
+        this.rear.next = newNode;
+        // set the newNode to the rear.
+        this.rear = newNode;
+      }
+      //add to size
+      this.size++;
+      return this.size;
     }
-
+  
+  
     /**
      * Removes and returns the first item of this queue.
      * - Time: O(n) linear, due to having to shift all the remaining items to
@@ -63,72 +78,79 @@ class Queue {
      * - Space: O(1) constant.
      * @returns {any} The data of the first item or undefined if empty.
      */
-    dequeue() {
-        if (this.isEmpty()) {
-            return undefined;
-        }
-
-        const removeData = this.front.data;
-        this.front = this.front.next;
-        this.size--;
-
-        if (this.isEmpty()) {
-            this.rear = null;
-        }
-        return removeData;
+    dequeue(){
+      if(this.isEmpty()){
+        return undefined;
+      }
+      const removed = this.front.data;
+      this.front = this.front.next
+      if (!this.front) {
+        this.rear = null;
+      }
+      return removed;
     }
-
+  
     // bonus
-    /**
-     * Check if the target value exists in the queue using the  operations
-     * @returns {Boolean }
+      /**
+     * Check if the target value exists in the queue using the basic queueoperations (w/o contains)
+     * @returns {Boolean } 
      */
-    contains(targetVal) {
-        // Create a copy of the queue to preserve its original state
-        const tempQueue = new Queue();
-
-        while (!this.isEmpty()) {
-            const frontItem = this.dequeue();
-
-            // Check if the front item is equal to the target value
-            if (frontItem === targetVal) {
-                // Restore the original state of the queue
-                while (!tempQueue.isEmpty()) {
-                    this.enqueue(tempQueue.dequeue());
-                }
-                return true;
-            }
-
-            tempQueue.enqueue(frontItem);
+    contains(targetVal) { //taking from one queue, adding (pushing q1's values) to (new) temp queue, iterating temp to see if it matches target
+      let isMatch = false;
+      const temp = new Queue();
+      while(!this.isEmpty()){
+        //dequeue
+        const val = this.dequeue();
+        temp.enqueue(val)
+        
+        if(val == targetVal) {
+          isMatch = true;
         }
-
-        // Restore the original state of the queue
-        while (!tempQueue.isEmpty()) {
-            this.enqueue(tempQueue.dequeue());
-        }
-
-        return false;
+      }
+      
+      while(!temp.isEmpty()) {
+        const tempData = temp.dequeue();
+        this.enqueue(tempData); 
+      }
+      
+      return isMatch;
+      
     }
-
-    printQueue() {
-        //for learning puspose
-        console.log("Front: " + this.front.data);
-        var runner = this.front;
-        while (runner) {
-            console.log(runner.data);
-            runner = runner.next;
+  
+      printQueue(){ //for learning purpose
+        if(this.isEmpty()){
+          console.log("Empty Queue")
+          return
         }
-        console.log("Rear: " + this.rear.data);
+      console.log("Front: " + this.front.data);
+      var runner = this.front;
+      while(runner){
+        console.log(runner.data)
+        runner= runner.next;
+      }
+      console.log("Rear: " +this.rear.data);
     }
-}
-
-var q = new Queue();
-q.enqueue("a");
-q.enqueue("b");
-q.enqueue("c");
-q.enqueue("d");
-
-q.printQueue(); //expected: front: "a", rear : "d"
-
-q.dequeue();
-q.printQueue(); //expected: front: "b", rear : "d"
+  }
+  
+  
+  var q = new Queue();
+  console.log(q.isEmpty())
+  q.enqueue("a");
+  q.enqueue("b");
+  q.enqueue("c");
+  q.enqueue("d");
+  
+  q.printQueue(); //expected: front: "a", rear : "d"
+  
+  q.dequeue(); 
+  q.printQueue(); //expected: front: "b", rear : "d"
+  console.log("======== Contains Tests ========");
+  console.log(q.contains("c")) // expected: true
+  q.printQueue()
+  
+  console.log(q.contains("x")) // expected: false
+  
+  
+  
+  
+  
